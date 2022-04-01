c0cd69da82015af4ab7b3d55ff6a0c57ebc26b09
Jardel Sousa <70775491+Smollky7@users.noreply.github.com>
2022-04-01 18:53:39 +0000

    working

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const express = require('express');
-const app = express();
-app.get('/', (request, response) => {
-	const ping = new Date();
-	ping.setHours(ping.getHours() - 3);
-	console.log(
-		`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
-	);
-	response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-
-
 const MongoClient = require('mongodb').MongoClient
 const MongoDBProvider = require('commando-provider-mongo').MongoDBProvider
 
